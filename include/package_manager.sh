353d536cf050006db0c40f05bde990eaa1be740a
lu <pena.m.luis@gmail.com>
2017-01-20 00:03:53 -0800

    YUM Interface 90% done

--- include/package_manager.sh
+++ include/package_manager.sh
@@ -20,46 +20,46 @@ function determine_manager {
   _ID=
 
   if [ ! -z "$_YUM" ];
-  	then 
+  	then
   		echo "Using YUM as package manager"
-  		
+
   		export _PACKAGE="$_YUM"
   		export _ID=0
-  
+
   elif [ ! -z "$_APT" ];
-  	then 
+  	then
   		echo "Using YUM as package manager"
-  		
+
   		export _PACKAGE="$_APT"
   		export _ID=1
 
   elif [ ! -z "$_PAC" ];
-  	then 
+  	then
   		echo "Using Pacmaan as package manager"
-  		
+
   		export _PACKAGE="$_PAC"
   		export _ID=2
- 
+
   elif [ ! -z "$_EME" ];
-  	then 
+  	then
   		echo "Using EMERGE as package manager"
-  		
+
   		export _PACKAGE="$_EME"
   		export _ID=3
   		$_PACKAGE "--version"
-  		
- 
+
+
   else
   		echo "Package Manager Not Found"
-  
-  fi 
+
+  fi
 
 
 
 }
 
 
-function package_install { 
+function package_install {
 	determine_manager
 
 	case $_ID in
@@ -68,7 +68,7 @@ function package_install {
     1) install_apt ; press_enter ;;
     2) install_pac ; press_enter ;;
     3) install_eme ; press_enter ;;
-  
+
     0) exit ;;
     *) echo " Package Manager Not Found";
 
@@ -78,36 +78,68 @@ function package_install {
 
 }
 
-function install_yum { 
+function install_yum {
 
-	echo "Using YUM"
-	source ./include/src/yum_functions.sh
+	source ./include/src/pkg/yum_functions.sh
+
+    _YUMCHOICE=
+    echo -e "\n "
+    echo "YUM Package Manager Interface";
+
+    echo "1) Update Every Package ";
+    echo "2) Install A Package ";
+    echo "3) Remove A Package";
+    echo "4) Sync Repos";
+    echo "5) Install Local RPM";
+    echo "6) Remove Local RPM"
+    echo "7) Install Developement Tools (GCC, other compilers and build tools)";
+    echo "8) Install Only Security Updates";
+
+
+
+  read _YUMCHOICE
+
+  case $_YUMCHOICE in
+
+      1) yum_all; press_enter ;;
+      2) yum_install; press_enter ;;
+      3) yum_remove ; press_enter ;;
+      4) yum_sync ; press_enter ;;
+      5) yum_local; press_enter;;
+      6) yum_rmlocal; press_enter;;
+      7) yum_devel; press_enter;;
+      8) yum_sec; press_enter;;
+
+      0) exit ;;
+      *) echo "Enter a digit above and try not to break this program.";
+
+        esac
 }
 
 
-function install_apt { 
+function install_apt {
 
 	echo "Using APT"
-	source ./include/src/apt_functions.sh
+	source ./include/src/pkg/apt_functions.sh
 }
 
 
 
-function install_pac { 
+function install_pac {
 
 	echo "Using pac"
-	source ./include/src/pac_functions.sh
+	source ./include/src/pkg/pac_functions.sh
 }
 
 
-function install_eme { 
+function install_eme {
 
 	source ./include/src/pkg/eme_functions.sh
 
-	EMECHOICE= 
+	EMECHOICE=
 	echo "Using EMERGE"
     echo "Gentoo's Package Manager Interface";
- 
+
 
     echo "1) Update Every Package ";
     echo "2) Install A Package ";
@@ -126,8 +158,8 @@ case $_EMECHOICE in
     2) eme_install; press_enter ;;
     3) eme_remove ; press_enter ;;
    	4) eme_sync ; press_enter ;;
-  
-    
+
+
     0) exit ;;
     *) echo "Enter a digit above and try not to break this program.";
 
@@ -136,9 +168,9 @@ case $_EMECHOICE in
 
 }
 
-function main { 
+function main {
 
-	
+
 	package_install
 
 }
--- include/src/pkg/yum_functions.sh
+++ include/src/pkg/yum_functions.sh
@@ -2,3 +2,66 @@
 # Author  : Luis M Pena
 # Date    : 1/18/2017
 # Purpose : RHEL YUM Package Manager Functions.
+
+function yum_all {
+
+  echo "YUM Will now update everything"
+  yum update -y
+}
+
+function yum_install {
+
+  echo "Type in Package you would like to install"
+  _YUMIN=
+  read _YUMIN
+  yum install "$_YUMIN" -y
+}
+
+function yum_remove {
+
+  echo "YUM will now remove package and configuration files"
+  _YUMOUT=
+  read _YUMOUT
+  yum remove "$_YUMOUT"
+
+}
+
+function yum_sync {
+  echo  "YUM will now resync repos"
+  yum check-update
+}
+
+function yum_local {
+
+  echo "Type in local path to RPM"
+  _RPMPATH=
+  read _RPMPATH
+
+  rpm -ivh "$_RPMPATH"
+ #-i to install , v to print verbose (-V to verify) and h to print the has.
+}
+
+function yum_rmlocal {
+
+  echo "Type RPM install name"
+  _RPMRMPATH=
+  read _RPMRMPATH
+
+  rpm -e "$_RPMRMPATH"
+
+}
+
+function yum_devel {
+
+  echo "YUM will install Development Build tools required by programs like OSSEC"
+  yum groupinstall "Development Tools"  --setopt=group_package_types=mandatory,default,optional
+}
+
+function yum_sec {
+
+  echo "YUM will now install only security updates"
+  yum update --security || yum update-minimal --security
+
+
+
+}
